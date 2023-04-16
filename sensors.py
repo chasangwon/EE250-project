df41026fcdf5970a22ca3b37e7644c6933497fb7
Sangwon Cha <sangwoncha@Sangwons-MBP.lan>
2023-04-15 22:41:20 -0700

    first commit

---
+++ sensors.py
@@ -0,0 +1,89 @@
+import time
+import grovepi
+import paho.mqtt.client as mqtt
+# import Adafruit_GPIO.SPI as SPI
+# import Adafruit_MCP3008
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print(
+        "Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
+def main():
+    # Specify port numbers for each sensor/display
+    temphum_port = 2  # port D2
+    ultra_port = 4  # port D4
+    # Establish ports as either inputs or outputs
+    grovepi.pinMode(ultra_port, "INPUT")
+
+
+    # # Initialize variables used in while loop
+    # humi_old = 0
+    # temp_old = 0
+
+    data = []
+
+    grovepi.set_bus("RPI_1")
+    time.sleep(1)
+
+    # client = connect_MQTT()
+
+    while True:
+
+        # Read new sensor value from potentiometer, ultrasonic ranger
+        distance_new = grovepi.ultrasonicRead(ultra_port)
+        [humi_new, temp_new] = grovepi.dht(temphum_port, 0)
+
+        print('Distance: {}, Humi: {}, Temp: {}'.format(distance_new, humi_new, temp_new))
+        # data.extend([distance_new, temp_new, humi_new])
+        #
+        # publish_data(client, data)
+
+        # New sensor values are now old
+        # [humi_old, temp_old] = [humi_new, temp_new]
+        # distance_old = distance_new
+        data = []
+    time.sleep(3)
+
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+def connect_MQTT():
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+
+    client.loop_start()
+    time.sleep(1)
+
+    return client
+
+
+def publish_data(mqtt_client, data):
+    distance = data[0]
+    temp = data[1]
+    hum = data[2]
+
+    # replace user with your USC username in all subscriptions
+    mqtt_client.publish("sangwonc/distance", f"{distance}")
+    print(f"Publishing distance: {distance} cm")
+    time.sleep(1)
+
+    # publish date and time in their own topics
+    mqtt_client.publish("sangwonc/temperature", f"{temp}")
+    print(f"Publishing temperature: {temp} C")
+    time.sleep(1)
+
+    mqtt_client.publish("sangwonc/humidity", f"{hum}")
+    print(f"Publishing humidity: {hum}%")
+    time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ subscribe.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+
+    print("Connected to server (i.e., broker) with result code " + str(rc))
+    # replace user with your USC username in all subscriptions
+    # Add the custom callbacks by indicating the topic and the name of the callback handle
+    client.subscribe("sangwonc/distance")
+    client.message_callback_add("sangwonc/distance", on_message_from_distance)
+
+    client.subscribe("sangwonc/temperature")
+    client.message_callback_add("sangwonc/temperature", on_message_from_temperature)
+
+    client.subscribe("sangwonc/humidity")
+    client.message_callback_add("sangwonc/humidity", on_message_from_humidity)
+
+
+def on_message(client, userdata, msg):
+    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+
+
+# Custom message callback.
+def on_message_from_distance(client, userdata, message):
+    print("Custom callback  - distance: " + message.payload.decode())
+
+def on_message_from_temperature(client, userdata, message):
+    print("Custom callback  - temperature " + message.payload.decode())
+
+def on_message_from_humidity(client, userdata, message):
+    print("Custom callback  - humidity " + message.payload.decode())
+
+
+if __name__ == '__main__':
+    # create a client object
+    client = mqtt.Client()
+    # attach a default callback which we defined above for incoming mqtt messages
+    client.on_message = on_message
+    # attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+
+    client.connect(host="68.181.32.115", port=11000, keepalive=60)
+
+    client.loop_forever()
