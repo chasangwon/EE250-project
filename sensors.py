ca85f83886f24325ef32fb0defc42fa8a9035452
Sangwon Cha <sangwoncha@Sangwons-MBP.lan>
2023-04-23 16:33:42 -0700

    thingsboard

--- sensors.py
+++ sensors.py
@@ -2,42 +2,24 @@ import time
 import grovepi
 import paho.mqtt.client as mqtt
 import json
+from tb_gateway_mqtt import TBDeviceMqttClient
+
 # import Adafruit_GPIO.SPI as SPI
-# import Adafruit_MCP3008
 
-try:
-    import RPi.GPIO as GPIO
-except RuntimeError:
-    print(
-        "Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
-
+client = None
+telemetry = {'Distance': 0, 'Temp': 0, 'Hum': 0}
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
 def connect_MQTT():
-    client = mqtt.Client()
-
-    client.on_connect = on_connect
-
-    client.connect(host="172.20.10.2", port=1883, keepalive=60)
-
-    client.loop_start()
+    global client
+    client = TBDeviceMqttClient('demo.thingsboard.io', 1883, 'Y3FuVTzm2QzLnA2il9Xo')
+    # client.on_connect = on_connect
+    client.connect()
     time.sleep(1)
 
-    return client
-
-
-def publish_data(mqtt_client, data):
-    distance = data[0]
-    temp = data[1]
-    hum = data[2]
-    d = {'Distance': distance, 'Temp': temp, 'Hum': hum}
-
-    # replace user with your USC username in all subscriptions
-    mqtt_client.publish("sangwonc/sensor_info", json.dumps(d))
-    print("Publishing data")
-    time.sleep(1)
+def publish_data():
 
     # # publish date and time in their own topics
     # mqtt_client.publish("sangwonc/temperature", f"{temp}")
@@ -49,39 +31,34 @@ def publish_data(mqtt_client, data):
     # time.sleep(1)
 
 def main():
+    global telemetry
     # Specify port numbers for each sensor/display
     temphum_port = 2  # port D2
     ultra_port = 4  # port D4
     # Establish ports as either inputs or outputs
     grovepi.pinMode(ultra_port, "INPUT")
 
-
     # # Initialize variables used in while loop
     # humi_old = 0
     # temp_old = 0
 
-    data = []
-
     grovepi.set_bus("RPI_1")
     time.sleep(1)
 
-    clientmqtt = connect_MQTT()
+    connect_MQTT()
 
-    while True:
+    while not client.stopped:
 
         # Read new sensor value from potentiometer, ultrasonic ranger
         distance_new = grovepi.ultrasonicRead(ultra_port)
         [humi_new, temp_new] = grovepi.dht(temphum_port, 0)
-
         print('Distance: {}, Humi: {}, Temp: {}'.format(distance_new, humi_new, temp_new))
-        data.extend([distance_new, temp_new, humi_new])
 
-        publish_data(clientmqtt, data)
+        telemetry['Distance'] = distance_new
+        telemetry['Temp'] = temp_new
+        telemetry['Hum'] = humi_new
+        client.send_telemtry(telemetry)
 
-        # New sensor values are now old
-        # [humi_old, temp_old] = [humi_new, temp_new]
-        # distance_old = distance_new
-        data = []
         time.sleep(1)
 
 if __name__ == '__main__':
